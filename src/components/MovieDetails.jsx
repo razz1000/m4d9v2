3b8c1c951e54d8fca962c07453f88b5ab883897c
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-04-28 15:52:03 +0100

    small css updates

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -2,12 +2,12 @@ import React from "react"
 import {Container, Row, Col} from "react-bootstrap"
 import { useParams, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import {Card, ListGroup, ListGroupItem, Button} from "react-bootstrap"
+import {Card, ListGroup, ListGroupItem, Button, Badge} from "react-bootstrap"
 import {Link} from "react-router-dom"
 
 const MovieDetails = () => {
     const params = useParams()
-    const navigate = useNavigate()
+
 
     const [selectedMovie, setSelectedMovie] = useState([])
     
@@ -38,23 +38,27 @@ const MovieDetails = () => {
     return (
         <Container>
             <Row className="justify-content-center my-3">
-                <Col className="col-12" style={{display: "flex", justifyContent: "center"}}>
-                <Card style={{ width: '18rem' }}>
+                <Col className="col-10" style={{display: "flex", justifyContent: "center"}}>
+                <Card >
   <Card.Img variant="top" src={selectedMovie.Poster} />
   <Card.Body>
-    <Card.Title>{selectedMovie.Title}</Card.Title>
+    <Card.Title>{selectedMovie.Title}   <Badge variant="warning">imdb Rating: {selectedMovie.imdbRating}</Badge>      </Card.Title>
     <Card.Text>
-            {selectedMovie.Writer}
+ 
     </Card.Text>
   </Card.Body>
   <ListGroup className="list-group-flush">
-    <ListGroupItem>Year: {selectedMovie.Year}</ListGroupItem>
+    <ListGroupItem>Year:<Badge pill variant="secondary">
+    {selectedMovie.Year}
+  </Badge></ListGroupItem>
     <ListGroupItem>Actors: {selectedMovie.Actors}</ListGroupItem>
-    <ListGroupItem>imdbRating: {selectedMovie.imdbRating}</ListGroupItem>
+    <ListGroupItem>Money generated:  <Badge pill variant="success">
+    {selectedMovie.BoxOffice}
+  </Badge></ListGroupItem>
   </ListGroup>
   <Card.Body>
       <Link to="/">
-    <Button variant="primary">Take me back please</Button>
+    <Button variant="success" size="lg" block>Take me back please</Button>
     </Link>
   </Card.Body>
 </Card>
